ba19c26f670ff69cab036db88a275334cc34ce34
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2023-05-21 20:54:19 +0300

    d12

---
+++ src/main/java/day12forloopwhileloop/ForLoop03.java
@@ -0,0 +1,51 @@
+package day12forloopwhileloop;
+
+import java.util.Scanner;
+
+public class ForLoop03 {
+
+    public static void main(String[] args) {
+
+        /*
+            Example 1: Asagidaki sekli cizdiren kodu yaziniz
+                * * * *
+                * * *
+                * *
+                *
+        */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Satir(row) sayisini giriniz : ");
+        int row = input.nextInt();
+
+
+        //0. yol gercek kendim
+        for (int i = 1; i <= row; i++) {
+            for (int j = row+1-i; j > 0 ; j--) {
+
+                    System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+//
+//
+//
+//        //1.Yol - Kendim
+//        for (int i = row; i>-1; i--){
+//            for (int j=1; j<=i; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        //2.Yol - Süleyman Hoca
+//        for (int i=1; i<=row; i++){
+//            for (int k=row; k>=i; k--){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+    }
+}
