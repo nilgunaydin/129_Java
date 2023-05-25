a2d83fa1f07531111985f796de69031e0825a1d7
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2023-05-25 16:39:45 +0300

    a little change

--- src/main/java/day16multidimensionalarrays/Star.java
+++ src/main/java/day16multidimensionalarrays/Star.java
@@ -14,14 +14,14 @@ public class Star {
                * * * * *
         */
 
-        int row = 5;
+        int row = 9;
         for (int i = 1; i <= row; i++) {
             for (int k = i; k < row; k++) {
                 System.out.print(" ");
             }
 
-            for (int m = 1; m <= 2 * i - 1; m++) {
-                if (i == row || m == 1 || m == 2 * i - 1) {
+            for (int m = 1; m <= 2 * i - 1; m++) {//2 * i - 1 == 9
+                if ((i == row || m == 1 || m == 2 * i - 1)&& m%2==1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
